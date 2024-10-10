e35c635bcfaeeed52f57458bd5b163c0f05ed120
Gabriel Vieira Caldana <133691141+GabrielVieira19@users.noreply.github.com>
2024-10-10 10:17:30 -0300

    Update
    Lastest version

--- Predict_teste.py
+++ Predict_teste.py
@@ -2,11 +2,14 @@ from ultralytics import YOLO
 import os 
 
 # Usamos o modelo que foi treinado, pegando os melhores resultados "best.pt"
-model = YOLO ('runs/detect/yolov8nbatch5/train/weights/best.pt')
+model = YOLO ('runs/detect/yolov8nbatch5/train2/weights/best.pt')
 
 # Rodamos o YOLO a fim de realizar a detcção em fotos ou vídeos 
-source_path = "Imagens_cap\Capturas_Camera\Camera_CV6%_1_Recorte.avi" 
-results = model(source=source_path, save=True, max_det=1, save_txt = True)
+for video in os.listdir('Imagens_cap/Capturas_Hikivision') :   
+    source_path = 'Imagens_cap/Capturas_Hikivision/' + video
+    output_dir = 'runs/detect/predict2' 
+    output_name = 'Predict_' + video[:-4]
+    results = model(source=source_path, save=True, max_det=1, save_txt = True, project=output_dir, name=output_name)
 
 
 
